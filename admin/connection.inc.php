a214da4ee6318f11b8943673f9b3bd822bd17c99
ajit9639 <61927091+ajit9639@users.noreply.github.com>
2022-12-12 10:59:46 +0530

    done

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -9,6 +9,6 @@ if($_SERVER['HTTP_HOST']=='localhost')
 }
 else
 {
-    $conn = mysqli_connect('localhost','pesanlqp_db','207.174.213.150','pesanlqp_db');
+    $connection = mysqli_connect('localhost','pesanlqp_db','207.174.213.150','pesanlqp_db');
 }
 ?>
